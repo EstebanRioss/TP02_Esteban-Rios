0107e499ae31e4c7b566d2f070765f6e9f4a7073
Esteban <Esteban@PC>
2024-04-25 14:53:32 -0300

    avance ejercicio01

--- TP02_EstebanRios/src/ar/edu/unju/fi/ejercicio1Main/Main.java
+++ TP02_EstebanRios/src/ar/edu/unju/fi/ejercicio1Main/Main.java
@@ -1,5 +1,7 @@
 package ar.edu.unju.fi.ejercicio1Main;
 
+import ar.edu.unju.fi.ejercicio1Model.Producto;
+
 public class Main {
 
 	public static void main(String[] args) {
---
+++ TP02_EstebanRios/src/ar/edu/unju/fi/ejercicio1Model/Producto.java
@@ -0,0 +1,5 @@
+package ar.edu.unju.fi.ejercicio1Model;
+
+public class Producto {
+
+}
