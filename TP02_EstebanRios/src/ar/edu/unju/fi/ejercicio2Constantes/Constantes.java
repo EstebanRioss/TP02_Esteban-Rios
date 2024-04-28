4eeb8de6ce635c3db2c78c84b84a6d27967e3d9a
Esteban <Esteban@PC>
2024-04-27 21:01:14 -0300

    Ejercicio 2 resuelto Esteban Rios

---
+++ TP02_EstebanRios/src/ar/edu/unju/fi/ejercicio2Constantes/Constantes.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.ejercicio2Constantes;
+
+public class Constantes {
+	
+	public enum Mes{
+		ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMBRE, OCTUBRE, NOVIEMBRE,DICIEMBRE
+	}
+	private int mes;
+	public int getMes() {
+		return mes;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	
+}
